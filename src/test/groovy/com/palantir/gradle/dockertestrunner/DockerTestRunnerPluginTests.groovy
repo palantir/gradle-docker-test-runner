fe554dd863129d67a8ff3d6ec0a4004b35a57bf2
Nick Miyake <nmiyake@palantir.com>
2016-04-04 17:54:19 -0700

    Update year in copyright notices and fix formatting

--- src/test/groovy/com/palantir/gradle/dockertestrunner/DockerTestRunnerPluginTests.groovy
+++ src/test/groovy/com/palantir/gradle/dockertestrunner/DockerTestRunnerPluginTests.groovy
@@ -1,6 +1,5 @@
-
 /*
- * Copyright 2015 Palantir Technologies
+ * Copyright 2016 Palantir Technologies
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.palantir.gradle.dockertestrunner
 
 import org.apache.commons.io.FileUtils
